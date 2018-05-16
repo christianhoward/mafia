39222efc0095365474f0495080aa5b07ee9e435d
christianhoward <christian.john.howard@gmail.com>
2018-05-16 13:30:23 -0400

    Modified CSS for player eliminations so that the X over a player's face is uniform at all screen sizes.

--- client/src/components/PlayersPanel.js
+++ client/src/components/PlayersPanel.js
@@ -3,7 +3,7 @@ import React from 'react';
 const PlayerPanel = ({ players, username }) => (
     players.map(player => {
         return (
-            <div key={player.username}>
+            <div className="player" key={player.username}>
                 <div className={player.eliminated ? 'player-out' : '' }><img src="http://via.placeholder.com/125x125" alt={player.username} /></div>
                 <div style={player.username === username ? {color: 'blue'} : {color: 'black'}}>{player.username}</div>
             </div>
