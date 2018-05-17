37bdb56163b25fb806cb7f5ffc0af84481a2df83
christianhoward <christian.john.howard@gmail.com>
2018-05-17 13:46:12 -0400

    Updated ChatPanel and ChatInput to disable chat when no talking is allowed.

--- client/src/components/ChatComponents/ChatInput.js
+++ client/src/components/ChatComponents/ChatInput.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-const ChatInput = ({ text, username, handleTextChange }) => (
-    <div className="chat-bar">
-        <input type="text" value={text} placeholder="Message" onChange={handleTextChange} onKeyDown={handleTextChange} className="chat-bar__input" />
+const ChatInput = ({ text, username, handleTextChange, chatLocked }) => (
+    <div className='chat-bar'>
+        <input type="text" value={text} placeholder="Message" onChange={handleTextChange} onKeyDown={handleTextChange} disabled={chatLocked} className="chat-bar__input" />
     </div>
 );
 
--- client/src/components/ChatPanel.js
+++ client/src/components/ChatPanel.js
@@ -13,16 +13,18 @@ class Chat extends Component {
         this.handleTextChange = this.handleTextChange.bind(this);
     }
     handleTextChange(e) {
-        if (e.keyCode === 13 && this.state.text.trim() !== '') {
-            const payload = {
-                username: this.props.username,
-                message: this.state.text.trim(),
-                timeStamp: new Date().toLocaleDateString(navigator.language, { hour: '2-digit', minute: '2-digit' })
-            };
-            axios.post('/send-message', payload);
-            this.setState({ text: '' });
-        } else {
-            this.setState({ text: e.target.value });
+        if (this.props.chatLocked === false) {
+            if (e.keyCode === 13 && this.state.text.trim() !== '') {
+                const payload = {
+                    username: this.props.username,
+                    message: this.state.text.trim(),
+                    timeStamp: new Date().toLocaleDateString(navigator.language, { hour: '2-digit', minute: '2-digit' })
+                };
+                axios.post('/send-message', payload);
+                this.setState({ text: '' });
+            } else {
+                this.setState({ text: e.target.value });
+            }
         }
     }
     render() {
@@ -30,9 +32,10 @@ class Chat extends Component {
             <div className="chat">
                 <ChatList chats={this.props.chats} />
                 <ChatInput
-                text={this.state.text}
-                username={this.props.username}
-                handleTextChange={this.handleTextChange}
+                    text={this.state.text}
+                    username={this.props.username}
+                    handleTextChange={this.handleTextChange}
+                    chatLocked={this.props.chatLocked}
                 />
             </div>
         );
