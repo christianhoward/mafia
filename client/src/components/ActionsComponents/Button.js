834ef5d5e679d6fda8e19a799163072658601c5b
christianhoward <christian.john.howard@gmail.com>
2018-05-18 16:21:43 -0400

    Updated Button component to take children prop when necessary.

--- client/src/components/ActionsComponents/Button.js
+++ client/src/components/ActionsComponents/Button.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const Button = ({ disabled, value, onClick, use }) => (
-        <button data-use={use} value={value} disabled={!disabled ? false : disabled} onClick={onClick}>{value}</button>
+const Button = ({ disabled, value, onClick, use, children }) => (
+        <button data-use={use} value={value} disabled={!disabled ? false : disabled} onClick={onClick}>{value !== children && children ? children : value}</button>
 );
 
 export default Button;
