80c75115bcf1d9fdb28648d352d2fd9823198256
christianhoward <christian.john.howard@gmail.com>
2018-05-20 13:36:50 -0400

    Removed commented out code.

--- client/src/components/ActionsComponents/DoctorVote.js
+++ client/src/components/ActionsComponents/DoctorVote.js
@@ -2,19 +2,6 @@ import React, { Component } from 'react';
 
 import Button from './Button';
 
-// const DoctorVote = ({ players, submitVote, doctorSaved }) => (
-//     <div>
-//         <div>You saved <strong>{doctorSaved ? doctorSaved.toUpperCase() : 'NO ONE'}</strong> last night.</div>
-//         {players.filter(player => player.eliminated === false).map(player => {
-//             return (
-//                 <div key={player.username}>
-//                     <Button use='Doctor' value={`Save ${player.username}`} onClick={submitVote} disabled={doctorSaved === player.username} />
-//                 </div>
-//             );
-//         })}
-//     </div>
-// );
-
 class DoctorVote extends Component {
     constructor(props) {
         super(props);
